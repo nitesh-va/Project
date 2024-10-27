dd822a163644a9c8b001ca1c432e4da91d313a83
Nitesh-va <nitesh.kumar@vaisesika.in>
2024-10-27 19:52:03 +0530

    Updated

--- Django/school/admin.py
+++ Django/school/admin.py
@@ -3,5 +3,6 @@ from school.models import School
 from unfold.admin import ModelAdmin as UnfoldModelAdmin
 class SchoolAdmin(UnfoldModelAdmin):
     list_display = ['name', 'school_id', 'location']
+
 # Register your models here.
 admin.site.register(School, SchoolAdmin)
--- Django/student/admin.py
+++ Django/student/admin.py
@@ -4,4 +4,6 @@ from unfold.admin import ModelAdmin as UnfoldModelAdmin
 # Register your models here.
 class StudentAdmin(UnfoldModelAdmin):
     list_display = ['name', 'roll_no', 'maths_marks','physics_marks','chemistry_marks','total_marks','percentage','emp_id','dept_name']
+
+
 admin.site.register(Student,StudentAdmin)
